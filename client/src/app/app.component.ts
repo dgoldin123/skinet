9a6cfd13996be36c38c20121034f4a7c38de6c2e
yzidell <yzidell2@hotmail.com>
2022-01-17 14:27:51 -0500

    end of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'SkiNet';
+  products: IProduct[];
+  mySub: Subscription;
+  
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(){
+    this.mySub = this.http.get('https://localhost:5001/api/products?pageSize=50')
+      .subscribe((response: IPagination) => {
+       this.products = response.data;
+       // console.log(response);
+      }, error => {
+        console.log(error);
+      }
+    );
+  }
+  
+  ngOnDestroy(){
+    this.mySub.unsubscribe();
+  }
+
 }
+
+/*
+
+{
+      "id": 0,
+      "name": "string",
+      "description": "string",
+      "price": 0,
+      "pictureUrl": "string",
+      "productType": "string",
+      "productBrand": "string"
+    }
+  ]
+*/
