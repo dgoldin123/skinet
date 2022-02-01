fde33a117191c4d4af2147c022b114f3eada3773
yzidell <yzidell2@hotmail.com>
2022-02-01 14:37:23 -0500

    end of sect 18

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Core.Entities;
 using System.Reflection;
+using Core.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Data
 {
@@ -14,12 +16,12 @@ namespace Infrastructure.Data
         {
         }
 
-        public DbSet<Product> Products { get; set; }
-        //i added DbSet<Product>? so that it is nullable - remove later on
-
+        public DbSet<Product> Products { get; set; }//i added DbSet<Product>? so that it is nullable - remove later on
         public DbSet<ProductBrand> ProductBrands { get; set; }
-
         public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -33,11 +35,20 @@ namespace Infrastructure.Data
                     var properties = entityType.ClrType.GetProperties()
                         .Where(p => p.PropertyType == typeof(decimal));
                     
+                    var dateTimeProperties = entityType.ClrType.GetProperties()
+                        .Where(p => p.PropertyType == typeof(DateTimeOffset));
+
                     foreach (var property in properties)
                     {
                         modelBuilder.Entity(entityType.Name).Property(property.Name)
                             .HasConversion<double>();
                     }    
+                    
+                    foreach (var property in dateTimeProperties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name)
+                            .HasConversion(new DateTimeOffsetToBinaryConverter());
+                    }
                 }
             }
         }
